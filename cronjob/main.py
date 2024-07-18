8abbc3de9e6a694b23ee2cf36ae484aad8d05199
Sarvesh Raj <saraj@rippling.com>
2024-07-18 12:46:14 +0530

    print all unpaid taxes

--- cronjob/main.py
+++ cronjob/main.py
@@ -3,6 +3,7 @@ import redis
 r = redis.Redis(host="redis-service.learnk8s.svc.cluster.local", port=6379, db=0, decode_responses=True)
 
 allUnpaidTaxes = r.hgetall("unpaidtaxes")
+print(f"All unpaid taxes: {allUnpaidTaxes}")
 
 
 def payTax(employeeId: str, tax: float):
